99afad1663b4b960176238f018f6ff00e1f4c4b4
abdellahramadan <ramadanabdel24@gmail.com>
2022-07-19 08:37:01 +0100

    first commit

---
+++ src/Extension.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace Abdellahrk\BoltAuthor;
+
+use Bolt\Extension\BaseExtension; 
+
+class Extension extends BaseExtension
+{
+    public function getName()
+    {
+        return 'Abdellahrk BoltAuthor';
+    }
+}
